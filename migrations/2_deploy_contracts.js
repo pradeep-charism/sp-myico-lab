e375bacd2017ab7df04875a5d34a4ec369b3eb8b
pradeep-charism <pradeep.charism@gmail.com>
2019-08-09 13:42:24 +0800

    token transfer in rinkeby

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,10 @@ const MyToken = artifacts.require('./MyToken.sol');
 
 module.exports = async function (deployer, network, accounts) {
     const latestBlock = await web3.eth.getBlock('latest');
-    const openingTime = latestBlock.timestamp + 60;
+    const openingTime = latestBlock.timestamp + 300;
     const closingTime = openingTime + 86400 * 20;
     const rate = web3.utils.toBN(1000);
-    const wallet = accounts[1];
+    const wallet = accounts[0];
     const tokenInstance = await deployer.deploy(MyToken);
 
     await deployer.deploy(
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+ const HDWalletProvider = require('truffle-hdwallet-provider');
+ const infuraKey = "a33870dc0c9b413a82091bf5c2b5c3f8";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const fs = require('fs');
+ const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -46,6 +46,16 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+     },
+     rinkeby: {
+     provider: () => new HDWalletProvider(
+     mnemonic,
+     `https://rinkeby.infura.io/v3/${infuraKey}`),
+     network_id: 4,
+     gas: 3000000,
+     confirmations: 2,
+     timeoutBlocks: 200,
+     skipDryRun: true
      },
 
     // Another network with more advanced options...
